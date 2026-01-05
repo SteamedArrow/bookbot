9836281a2893b859444bf658b01c03c87fabe5bd
SteamedArrow <steamedarrow@gmail.com>
2026-01-04 19:21:51 -0600

    Course Complete

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
-from stats import count_words, count_characters
+from stats import count_words, get_chars_dict, sort_chars_dict
+import sys
 
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
-def main():
-    book = get_book_text('books/frankenstein.txt')
-    num_words = count_words(book)
-    char_count = count_characters(book)
+def display(filepath, num_words, dicts):
+    print("============ BOOKBOT ============")
+    print(f'Analyzing book found at {filepath}...')
+    print("----------- Word Count ----------")
     print(f'Found {num_words} total words')
-    print(char_count)
+    print("--------- Character Count -------")
+    for i in range(len(dicts)):
+        if dicts[i]["char"].isalpha():
+            print(f'{dicts[i]["char"]}: {dicts[i]["num"]}')
+    print("============= END ===============")
+
+def main():
+    if len(sys.argv) < 2 or len(sys.argv) > 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        filepath = sys.argv[1]
+
+    try:
+        book = get_book_text(filepath)
+        num_words = count_words(book)
+        chars_dict = get_chars_dict(book)
+        sorted_dicts = sort_chars_dict(chars_dict)
+
+        display(filepath, num_words, sorted_dicts)
+    except Exception as e:
+        print(f'Error encountered: {e}')
 
 main()
--- stats.py
+++ stats.py
@@ -5,11 +5,22 @@ def count_words(bookText):
         count += 1
     return count
 
-def count_characters(bookText):
-    character_dict = {}
+def get_chars_dict(bookText):
+    chars = {}
     for char in bookText:
-        if char.lower() not in character_dict:
-            character_dict[char.lower()] = 1
+        if char.lower() in chars:
+            chars[char.lower()] += 1
         else:
-            character_dict[char.lower()] += 1
-    return character_dict
+            chars[char.lower()] = 1
+    return chars
+
+def sort_on(items):
+    return items["num"]
+
+def sort_chars_dict(chars):
+    dict_list = []
+    for char in chars:
+        num = chars[char]
+        dict_list.append({"char": char, "num": num})
+    dict_list.sort(reverse=True, key=sort_on)
+    return dict_list
